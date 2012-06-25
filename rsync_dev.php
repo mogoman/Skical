051615d25eafc3efc05cb329cefd7f88f112cee6
Warren <warren@vanzoo.de>
2012-06-25 22:18:29 +0200

    removed files from git

--- rsync_dev.php
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-echo running rsync --exclude-from=rsync_exclude.txt -e "ssh -p22" -avz . www-data@10.211.56.3:/var/www/skical
-rsync --exclude-from=rsync_exclude.txt -e "ssh -p22" -avz . www-data@10.211.56.3:/var/www/skical
