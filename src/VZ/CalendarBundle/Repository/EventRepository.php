ac1cadbfbb1dc252104091637266fb83afaf5bab
Warren van der Woude <warren@vanzoo.de>
2012-06-26 14:10:22 +0200

    added registration and lost password templates, plus activated email registration

--- src/VZ/CalendarBundle/Repository/EventRepository.php
+++ src/VZ/CalendarBundle/Repository/EventRepository.php
@@ -7,16 +7,16 @@ use Doctrine\ORM\EntityRepository;
 class EventRepository extends EntityRepository
 {
     /**
-     * retrieve all events that are still happening in the future, order them by newest to oldest
+     * retrieve all events that are still happening in the future, order them showing the next one first
      *
-     * @return mixed
+     * @return DoctrineCollection
      */
     public function findAllCurrent()
     {
         $em = $this->getEntityManager();
 
         $query = $em->createQuery(
-            'SELECT e FROM VZCalendarBundle:Event e WHERE e.startDate >= CURRENT_DATE() ORDER BY e.startDate DESC'
+            'SELECT e FROM VZCalendarBundle:Event e WHERE e.startDate >= CURRENT_DATE() ORDER BY e.startDate'
         ); // ->setParameter('date', date('Y-m-d'));
 
         return $query->getResult();
